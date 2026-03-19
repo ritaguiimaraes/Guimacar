3f5f3afa30dc41bad08624b4680f046e54bf2845
ritaguiimaraes <ritamoreiraguimaraes@gmail.com>
2026-03-19 21:28:45 +0000

    mais coisas

---
+++ carros.js
@@ -0,0 +1,49 @@
+const stockGuimacar = [
+    {
+        id: 1,
+        marca: "Renault Megane",
+        modelo: "1.5 Dci 110 CV",
+        preco: "15.900 €",
+        mes: "Jan. 2018",
+        kms: "176.307",
+        combustivel: "Gasóleo",
+        fotos: [
+            "img/carro1/1.jpg",
+            "img/carro1/2.jpg",
+            "img/carro1/3.jpg",
+            "img/carro1/4.jpg",
+            "img/carro1/5.jpg"
+        ],
+        descricao: "Viatura em excelente estado, com revisões na marca..."
+    },
+    {
+        id: 2,
+        marca: "Audi",
+        modelo: "A3 Cabrio",
+        preco: "15.000 €",
+        mes: "Jun. 2012",
+        kms: "134.000",
+        combustivel: "Gasóleo",
+        fotos: [
+            "img/carro2/1.jpg",
+            "img/carro2/2.jpg",
+            "img/carro2/3.jpg"
+        ],
+        descricao: "Versão S-Line com capota elétrica e bancos desportivos..."
+    },
+    {
+        id: 3,
+        marca: "Audi",
+        modelo: "A3 Cabrio",
+        preco: "15.000 €",
+        mes: "Jun. 2012",
+        kms: "134.000",
+        combustivel: "Gasóleo",
+        fotos: [
+            "img/carro3/1.jpg",
+            "img/carro3/2.jpg",
+            "img/carro3/3.jpg"
+        ],
+        descricao: "Versão S-Line com capota elétrica e bancos desportivos..."
+    }
+];
